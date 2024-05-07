e53ed43359b4020a1fc3ab93c5204b5053118fa3
Alan Corrêa <souzaalanc75@gmail.com>
2024-05-07 13:40:22 -0300

    updating files

--- bootcamp-java/src/edu/alan/tiposevariaveis/TiposVariaveis.java
+++ bootcamp-java/src/edu/alan/tiposevariaveis/TiposVariaveis.java
@@ -1,7 +1,9 @@
 package edu.alan.tiposevariaveis;
 
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        int age = 23;
+        double salario = 2500.54;
         System.out.println("Hello, World!");
     }
 }
